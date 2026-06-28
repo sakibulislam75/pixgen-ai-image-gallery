fce3591760b6a066db1eee6265a5b36055edaa80
sakibulislam75 <sakibulislam1623@gmail.com>
2026-06-28 20:41:39 +0600

    feat: add banner & footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
+import Banner from '@/components/Banner';
 import Image from 'next/image';
 
 export default function Home() {
-   return <div></div>;
+   return (
+      <div className="w-10/12 mx-auto">
+         <Banner />
+      </div>
+   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,42 +4,45 @@ import Link from 'next/link';
 
 const Navbar = () => {
    return (
-      <div className="border-b px-2">
-         <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
-            <div className="flex gap-2 items-center">
+      <div className="border-b px-4 w-10/12 mx-auto ">
+         <nav className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between gap-4 py-3">
+            {/* Logo */}
+            <div className="flex items-center gap-2">
                <Image
-                  src={'/logo.png'}
+                  src="/logo.png"
                   alt="logo"
-                  loading="eager"
                   width={30}
                   height={30}
-                  className="object-cover h-auto w-auto"
+                  priority
+                  className="h-auto w-auto"
                />
-               <h3 className="font-black text-lg">pixgen.</h3>
+               <h3 className="text-lg font-black">pixgen.</h3>
             </div>
 
-            <ul className="flex items-center gap-5 text-sm">
+            {/* Navigation */}
+            <ul className="flex flex-wrap justify-center items-center gap-3 sm:gap-5 text-sm">
                <li>
-                  <Link href={'/'}>Home</Link>
+                  <Link href="/">Home</Link>
                </li>
                <li>
-                  <Link href={'/all-photos'}>All Photos</Link>
+                  <Link href="/all-photos">All Photos</Link>
                </li>
                <li>
-                  <Link href={'/pricing'}>Pricing</Link>
+                  <Link href="/pricing">Pricing</Link>
                </li>
                <li>
-                  <Link href={'/profile'}>Profile</Link>
+                  <Link href="/profile">Profile</Link>
                </li>
             </ul>
 
-            <div className="flex gap-4">
-               <ul className="flex items-center gap-4  text-sm">
+            {/* Auth */}
+            <div>
+               <ul className="flex items-center gap-4 text-sm">
                   <li>
-                     <Link href={'/signup'}>SignUp</Link>
+                     <Link href="/signup">SignUp</Link>
                   </li>
                   <li>
-                     <Link href={'/signin'}>SignIn</Link>
+                     <Link href="/signin">SignIn</Link>
                   </li>
                </ul>
             </div>
